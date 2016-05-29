637ceba06808746a5fe41c38746444e198545ea9
Edgar Merino <emerino@nuevebit.com>
2016-05-29 00:46:36 -0500

    actualizado template config

--- config.template.php
+++ config.template.php
@@ -8,9 +8,9 @@ use ConroeSoluciones\CSReporter\Credenciales;
 
 return array(
     // contrato con ConroeSoluciones
-    "csCredenciales" => new Credenciales(""),
+    "csCredenciales" => new Credenciales("RFC"),
     // credenciales de acceso al portal CFDI del SAT
-    "satCredenciales" => new Credenciales("", ""),
+    "satCredenciales" => new Credenciales("RFC", "pass"),
     // folio conocido para realizar búsquedas en los ejemplos
     "consultaFolio" => "",
 );
